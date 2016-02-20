be55177bf3aa4300330621fed913d33d189c804e
Erik Shreve <accounts@erikshreve.com>
2016-02-20 10:43:36 -0600

    Added args4j argument parsing See @Options near top of Runner class for description of optoins.

--- src/main/java/edu/utdallas/cs6301_502/Runner.java
+++ src/main/java/edu/utdallas/cs6301_502/Runner.java
@@ -1,8 +1,18 @@
+//***************************************************************************
+// Assignment: 2
+// Team : 2
+// Team Members: Ryan Wiles, Erik Shreve
+//
+// Code reuse/attribution notes:
+// args4j (for command line parsing) based on example code from:
+//   https://github.com/kohsuke/args4j/blob/master/args4j/examples/SampleMain.java
+// walkFolder method based on example code from:
+//   https://lucene.apache.org/core/5_4_1/demo/src-html/org/apache/lucene/demo/IndexFiles.html
+//***************************************************************************
 package edu.utdallas.cs6301_502;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
@@ -13,10 +23,14 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
@@ -28,51 +42,107 @@ import seers.methspl.MethodDoc;
 import seers.methspl.MethodSplitter;
 
 class Runner {
+	@Option(name="-d",usage="print debug information to console")
+	private boolean debug = false;
+		
+	@Option(name="-c",usage="create new index")
+	private static boolean create = false;
+
+	@Option(name="-src", usage="base folder containing .java source to index")
+	private String baseFolder = "";
+	
+	@Option(name="-i", usage="index location")
+	private String indexPath = "";
+	
+	@Option(name="-g", usage="gold set file")
+	private String goldSetFile ="";
+	
+    // receives other command line parameters than options
+    @Argument
+    private List<String> arguments = new ArrayList<String>();
+	
 
 	private static MethodSplitter splitter;
 	private static LuceneUtil luceneUtil;	
 	private TextScrubber textScrubber;	
 	private BugReports bugReports;
-
-	
-	private static boolean debug = false;
-	private static boolean create = true;
-
-	
+    
+    
 	public static void main(String... args) throws Exception {
-		String baseFolder = args[0];
-		String indexPath = args[1];
-		String goldSetFile = args[2];
-		
-		luceneUtil = new LuceneUtil(create, indexPath);
 
-		// the root folder where the code is located
-		// create the instance of the method splitter
-		splitter = new MethodSplitter(baseFolder);
-
-		System.out.println("Indexing to directory '" + indexPath + "'...");
-
-		Runner r = new Runner(true, goldSetFile);
-		r.walkFolder(Paths.get(baseFolder));
+		Runner r = new Runner();
+		r.doMain(args);
+		r.run();
 	}
 
-	public Runner(boolean debug, String goldSetFile) {
-		super();
-		this.debug = debug;
+	public void doMain(String[] args)
+	{
+		CmdLineParser parser = new CmdLineParser(this);
 
 		try {
+			// parse the arguments.
+			parser.parseArgument(args);
+			
+			// check if enough arguments are given.
+			if( goldSetFile.isEmpty() )
+				throw new CmdLineException(parser,"No gold set file was provided");
+			
+			if( indexPath.isEmpty() )
+				throw new CmdLineException(parser,"No index location was provided");
+			
+			if( create && baseFolder.isEmpty() )
+				throw new CmdLineException(parser,"No base folder was provided, but create new index was specified");
+
+		} catch( CmdLineException e ) {
+			// report an error message.
+			System.err.println(e.getMessage());
+			System.err.println("java Runner [options...] arguments...");
+			// print the list of available options
+			parser.printUsage(System.err);
+			System.err.println();
+
+			return;
+		}
+	}
+	
+	public Runner() {
+		super();
+	}
+
+	public void run()
+	{
+		try {
 			// SETUP
-			textScrubber = new TextScrubber(loadWords("stop_words.xml"), loadWords("java_keywords.xml"), 2);
+			luceneUtil = new LuceneUtil(create, indexPath);
+
+			if (!baseFolder.isEmpty())
+			{
+				// 	the root folder where the code is located
+				// create the instance of the method splitter
+				splitter = new MethodSplitter(baseFolder);
+
+				System.out.println("Indexing to directory '" + indexPath + "'...");
+			
+				textScrubber = new TextScrubber(loadWords("stop_words.xml"), loadWords("java_keywords.xml"), 2);
+				
+				walkFolder(Paths.get(baseFolder));
+			}
+			else
+			{
+				System.out.println("Using index at '" + indexPath + "'");
+			}
 			
 			bugReports = loadBugReports(goldSetFile);
+			
+			// TODO: RUN QUERIES			
+						
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
-
+	
 	private void debug(String line) {
-		if (Runner.debug) {
+		if (this.debug) {
 			System.out.println(line);
 		}
 	}
